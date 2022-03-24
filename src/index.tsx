8b518931b4bc8853dd3c7d201210b45636c2d6f3
Jane <badidea2004@gmail.com>
2022-03-23 19:55:09 -0700

    Terminology

--- src/index.tsx
+++ src/index.tsx
@@ -4,46 +4,46 @@ import { LinkGrid, LinkGridItem } from "./LinkGrid/LinkGrid";
 const contactLinkGridItems: Array<LinkGridItem> = [
     {
         imageURL: "res/art/blueberry.webp",
-        imageAlt: "blueberry",
-        link: "#"
+        imageAlt: "Twitter",
+        link: "https://twitter.com/bluebsCat"
     },
     {
-        imageURL: "res/art/blueberry.webp",
-        imageAlt: "blueberry",
-        link: "#"
+        imageURL: "res/art/email.webp",
+        imageAlt: "Email",
+        link: "mailto:support@fruitbasket.games"
     },
     {
-        imageURL: "res/art/blueberry.webp",
-        imageAlt: "blueberry",
-        link: "#"
+        imageURL: "res/art/itch.png",
+        imageAlt: "Itch",
+        link: "https://bluebstophat.itch.io/"
     },
     {
-        imageURL: "res/art/blueberry.webp",
-        imageAlt: "blueberry",
-        link: "#"
+        imageURL: "res/art/discord.png",
+        imageAlt: "Discord",
+        link: "https://discord.gg/SVT57Zb"
     },
 ]
 
 const coolPeopleLinkGridItems: Array<LinkGridItem> = [
     {
-        imageURL: "res/art/blueberry.webp",
-        imageAlt: "blueberry",
-        link: "#"
+        imageURL: "res/art/stork.webp",
+        imageAlt: "Storklabs",
+        link: "https://www.storklabs.io/"
     },
     {
-        imageURL: "res/art/blueberry.webp",
-        imageAlt: "blueberry",
-        link: "#"
+        imageURL: "res/art/knowl.jpg",
+        imageAlt: "Knight Owl Hoots",
+        link: "https://twitter.com/KnightOwlHoots"
     },
     {
-        imageURL: "res/art/blueberry.webp",
-        imageAlt: "blueberry",
-        link: "#"
+        imageURL: "res/art/controllerTGD.png",
+        imageAlt: "Trans Game Dev",
+        link: "https://www.transgame.dev/"
     },
     {
-        imageURL: "res/art/blueberry.webp",
-        imageAlt: "blueberry",
-        link: "#"
+        imageURL: "res/art/slow_arpeggio.webp",
+        imageAlt: "Slow Arpeggio",
+        link: "https://slowarpeggio.com"
     },
 ]
 
