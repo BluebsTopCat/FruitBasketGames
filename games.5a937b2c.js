81b7e396ca2ebe9473dc8d45384018510a6b050d
BluebsTopCat <john@doe.org>
2022-10-29 13:40:41 -0500

    deploy

---
+++ games.5a937b2c.js
@@ -0,0 +1,2 @@
+var e=document.getElementById("background");window.addEventListener("scroll",(()=>{let n=function(e,n,t){let o=e/(n-t);return o/=1.1,2*o}(window.scrollY,document.body.scrollHeight,window.innerHeight);e.currentTime=n}));
+//# sourceMappingURL=games.5a937b2c.js.map
