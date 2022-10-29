8561e78679f8b726de99ff1bcf8e57fadbbb2e9c
BluebsTopCat <john@doe.org>
2022-10-29 18:15:17 -0500

    deploy

--- music.bd2560bb.js
+++
@@ -1,2 +0,0 @@
-var e=document.getElementById("background");window.addEventListener("scroll",(()=>{let n=function(e,n,t){let o=e/(n-t);return o/=1.1,2*o}(window.scrollY,document.body.scrollHeight,window.innerHeight);e.currentTime=n}));
-//# sourceMappingURL=music.bd2560bb.js.map
