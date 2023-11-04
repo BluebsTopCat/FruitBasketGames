4c591551ad12a8c8616dd5e9c9e8abbdebf079ee
Jane P <badidea2004@gmail.com>
2023-11-04 16:49:27 -0500

    deploy

--- index.b98eff2e.js
+++
@@ -1,2 +0,0 @@
-let n;window.onload=function(){n=document.getElementById("background")},window.addEventListener("scroll",(()=>{let e=(o=window.scrollY,d=document.body.scrollHeight,t=window.innerHeight,2*(o/(d-t)-.1));var o,d,t;n.currentTime=e}));
-//# sourceMappingURL=music.b98eff2e.js.map
