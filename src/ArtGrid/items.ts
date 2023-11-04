f149507003dc149dcd4192ca1ba39ced77b174ff
Jane P <badidea2004@gmail.com>
2023-11-04 18:04:17 -0500

    convert all to better format

--- src/ArtGrid/items.ts
+++ src/ArtGrid/items.ts
@@ -13,50 +13,47 @@ export interface ArtGridItem {
 }
 
 export const items: Array<ArtGridItem> = [
-    
     {
         type: ItemType.Video,
         title: "Soothshatter",
         description: "<b>3d Digital Media</b>\nAugust 2023\n<i>Independent Work</i>\n\nA husk of a person- their identity shattered to the winds. Made in Blender",
-        url: "res/art/shatter.mp4"
+        url: "res/art/shatter.webm"
     },
-    
     {
         type: ItemType.Image,
         title: "Lillian Keyton",
         description: "<b>3d Digital Media</b>\nApril 2023\n<i>Independent Work</i>\n\nFanart of Lillian Keyton, from the webcomic 'Soulwinder'. Made in Blender",
-        url: "res/art/Soulwinder.png"
+        url: "res/art/Soulwinder.webp"
     },
     {
         type: ItemType.Image,
         title: "Replacement",
         description: "<b>3d Digital Media</b>\nSeptember 2023\n<i>Independent Work</i>\n\nA fake advertisement for prosthetic upgrades. Made in Blender",
-        url: "res/art/molddroid.png"
+        url: "res/art/molddroid.webp"
     },
-    
     {
         type: ItemType.Image,
         title: "BC Tourism Poster",
         description: "<b>3d Digital Media</b>\nDecember 2022\n<i>Independent Work</i>\n\nA fake advertisement for the woods of BC. Made in Blender",
-        url: "res/art/TourismPosterBc.png"
+        url: "res/art/TourismPosterBc.webp"
     },
     {
         type: ItemType.Video,
         title: "Forgotten Church",
         description: "<b>3d Digital Media</b>\nJanuary 2023\n<i>Independent Work</i>\n\nA shattered point in space- a eulogy to the forgotton. Made in Blender",
-        url: "res/art/TheStars.mp4"
+        url: "res/art/TheStars.webm"
     },
     {
         type: ItemType.Image,
         title: "City Skyline",
         description: "<b>3d Digital Media</b>\nJune 2023\n<i>Independent Work</i>\n\nA city that never sleeps. Made in Blender",
-        url: "res/art/Skiview.png"
+        url: "res/art/Skiview.webp"
     },
     {
         type: ItemType.Video,
         title: "Silver Fanart",
         description: "<b>3d Digital Media</b>\nJuly 2023\n<i>Independent Work</i>\n\nFanart of Silver, from the 2d RPG Oneshot. Made in Blender",
-        url: "res/art/Silver3d.mp4"
+        url: "res/art/Silver3d.webm"
     },
     {
         type: ItemType.Video,
@@ -74,31 +71,31 @@ export const items: Array<ArtGridItem> = [
         type: ItemType.Video,
         title: "The Sky.",
         description: "<b>3d Digital Media</b>\nFebuary 2023\n<i>Independent Work</i>\n\nNobody has seen the stars in a hundred years. Made in Blender",
-        url: "res/art/TheSky.mp4"
+        url: "res/art/TheSky.webm"
     },
     {
         type: ItemType.Video,
         title: "Freefalling",
         description: "<b>3d Digital Media</b>\nSeptember 2022\n<i>Independent Work</i>\n\nFalling from the skyline. Made in Blender",
-        url: "res/art/Falling.mp4"
+        url: "res/art/Falling.webm"
     },
     {
         type: ItemType.Image,
         title: "Mage",
         description: "<b>3d Digital Media</b>\nAugust 2022\n<i>Independent Work</i>\n\nA mysterious alchemist hones her craft. Made in Blender",
-        url: "res/art/Render4.png"
+        url: "res/art/Render4.webp"
     },
     {
         type: ItemType.Video,
         title: "Fight Scene",
         description: "<b>3d Digital Media</b>\nJune 2023\n<i>Independent Work</i>\n\nPracticing animation and 2d effects with a fight scene! Made in Blender",
-        url: "res/art/FightScene.mp4"
+        url: "res/art/FightScene.webm"
     },
     {
         type: ItemType.Image,
         title: "Matsterna",
         description: "<b>3d Digital Media</b>\nJuly 2023\n<i>Independent Work</i>\n\nThe Creator of the cosmos. Made in Blender",
-        url: "res/art/Matsterna.png"
+        url: "res/art/Matsterna.webp"
     },
     {
         type: ItemType.Video,
@@ -110,7 +107,7 @@ export const items: Array<ArtGridItem> = [
         type: ItemType.Image,
         title: "Science Fiction Fantasy Alliance",
         description: "<b>2d Digital Media</b>\nApril 2022\n<i>SFFA</i>\n\n I designed and organized a group of members of reddit to place this design on https://reddit.com/r/place, an art experiment in which each user could place one pixel on a grid every 5 minutes. our work turned out to be one of the largest there was!",
-        url: "res/art/icon.png"
+        url: "res/art/icon.webp"
     },
     {
         type: ItemType.Image,
