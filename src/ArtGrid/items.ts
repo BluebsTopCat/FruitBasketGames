115e5a50d03ca918348a598e24ea6417f58ed3f6
Jane P <badidea2004@gmail.com>
2024-10-28 15:09:30 -0500

    wtryuiop[

--- src/ArtGrid/items.ts
+++ src/ArtGrid/items.ts
@@ -14,16 +14,53 @@ export interface ArtGridItem {
 
 export const items: Array<ArtGridItem> = [
     {
+        type: ItemType.Image,
+        title: "Office Concept Art",
+        description: "<b>3d Digital Media</b>\nDecember 2023\n<i>Independent Work</i>\n\nA potential design for an office complex abandoned in the 60's. Made in Blender",
+        url: "res/art/officetest.png"
+    },
+    {
+        type: ItemType.Image,
+        title: "Saprophytic Poster",
+        description: "<b>2d Digital Media</b>\nOctober 2024\n<i>Independent Work</i>\n\nAn experiment on what promotional art for a horror game I've been designing would look like. Painted in Clip Studio Paint",
+        url: "res/art/saprophyticposterformatted.png"
+    },
+    {
         type: ItemType.Video,
         title: "Soothshatter",
         description: "<b>3d Digital Media</b>\nAugust 2023\n<i>Independent Work</i>\n\nA husk of a person- their identity shattered to the winds. Made in Blender",
         url: "res/art/shatter.webm"
     },
     {
+        type: ItemType.Video,
+        title: "Maya",
+        description: "<b>3d Digital Media</b>\nOctober 2024\n<i>Independent Work</i>\n\nA 3d model of Maya, from the webcomic Kill Six Billion Demons- Rigged to support a full range of motion and facial expressions. Made in Blender",
+        url: "res/art/Maya.mp4"
+    },
+  
+    {
         type: ItemType.Image,
-        title: "Office Concept Art",
-        description: "<b>3d Digital Media</b>\nDecember 2023\n<i>Independent Work</i>\n\nA potential design for an office complex abandoned in the 60's. Made in Blender",
-        url: "res/art/officetest.png"
+        title: "Tarot Cards",
+        description: "<b>2d Digital Media</b>\nAugust 2024\n<i>Independent Work</i>\n\nA series of lighting studies made through the form of Tarot Cards. From top left to bottom right: The High Priestess, The Magician, The Empress, and The Fool.",
+        url: "res/art/tarotmerged.png"
+    },
+    {
+        type: ItemType.Image,
+        title: "Cover Art",
+        description: "<b>2d Digital Media</b>\nJuly 2024\n<i>Independent Work</i>\n\nCover art for a story I've been writing in my spare time. Painted from scratch in Clip Studio Paint.",
+        url: "res/art/parasiticinfluences.png"
+    },
+    {
+        type: ItemType.Video,
+        title: "Camcorder",
+        description: "<b>3d Digital Media</b>\nJune 2024\n<i>Independent Work</i>\n\nA 3d model of a fictional tape camcorder, made Game Ready. 4,449 Tris, with a 2048x2048 PBR texture set.",
+        url: "res/art/camcorder.webm"
+    },
+    {
+        type: ItemType.Video,
+        title: "Camcorder Viewmodel",
+        description: "<b>3d Digital Media</b>\nJune 2024\n<i>Independent Work</i>\n\nA series of viewmodel animations for said camcorder. The hand and rigging are also made by me.",
+        url: "res/art/viewmodelcam.webm"
     },
     {
         type: ItemType.Image,
